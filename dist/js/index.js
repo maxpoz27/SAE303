1c5d73268f5daaf60f993a7aa90c16be284da372
unknown <poz3011@mail.ru>
2022-10-10 18:23:45 +0200

    un peu d'ajout

--- dist/js/index.js
+++ dist/js/index.js
@@ -4,7 +4,7 @@ const footer = `
   <div class="container footer">
     <small><a href="">Mentions legales</a></small>
     <p> SAE303 Pozdnyakov, Vergeaud, Humeau </p>
-    <div class="reseaux">
+    <div class="reseaux row">
       <a href="#">
         <img src="../dist/img/instagram-32.svg" alt="LogoI">
       </a>
@@ -22,10 +22,12 @@ const footer = `
 </footer>
 `
 const articles = `
-  <article  v-for="article in articles">
+  <article :id="article.id"  v-for="article in articles">
     <img :src="article.image" alt="image d'article">
-    <p>{{ article.description }}   <a :href='article.path'>lire la suite</a></p>
-
+    <p>{{ article.description }}</p>
+    <a :key="article.id" :id="article.id" :href='article.path'>
+    lire la suite
+    </a>
   </article>
 `
 const app = Vue.createApp({
@@ -38,13 +40,13 @@ const app = Vue.createApp({
   /*html*/
   `
   <header class="container">
-  <div class="logo">
+  <div class="d-flex justify-content-center">
     <a href="index.html">
       <img src="../dist/img/logo-placeholder.png" alt="Logo">
     </a>
   </div>
-  <nav class="navbar">
-    <a class="btn" v-for="tab in tabs" :href="tab.path">{{tab.name}}</a>
+  <nav class="d-flex justify-content-between">
+    <a class="btn" v-for="tab in tabs"  :href="tab.path">{{tab.name}}</a>
   </nav>
   </header>
   `,
@@ -52,7 +54,7 @@ const app = Vue.createApp({
     return {
       tabs : [
         {name: 'Accueil', path: './index.html'},
-        {name: 'Actualites', path: './actualites.html'},
+        {name: 'Actualités', path: './actualites.html'},
         {name: 'Gallerie', path: './gallerie.html'},
         {name: 'Contact', path: './contact.html'}
       ]
@@ -63,15 +65,15 @@ const app = Vue.createApp({
   data() {
     return {
       articles: [
-        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#"},
-        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#"},
-        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#"},
-        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#"}
+        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#",id:"0"},
+        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#",id:"1"},
+        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#",id:"2"},
+        {image : "https://via.placeholder.com/1200x150/",description :"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",path:"#",id:"3"}
       ]
     }
   }
 }).component('foot',{
-  template : footer,
+  template : footer
 }).mount('body')
 
 
@@ -83,3 +85,20 @@ liens.each(function(){
     $(this).css({'text-decoration': 'underline'})
   }
 })
+
+
+
+let actualites = $('article')
+actualites.each(function(){
+  let article = $(this)
+  let lien = article.children('a')
+  let p = article.children('p')
+  lien.click(function(){
+    p.css('overflow','visible')
+    p.css('white-space','normal')
+    article.css('height','auto')
+  })
+})
+
+let reseaux = $('.reseaux')
+reseaux.children().css('width','auto')
--- view/footer.vue
+++
@@ -1,9 +0,0 @@
-<template id="">
-  <footer>
-    <p>JSP</p>
-  </footer>
-
-</template>
-
-
-<a class="tab" v-for="tab in tabs" :key="tab" :class="['tab-button', { active: currentTab === tab }]" @click="currentTab = tab" >l</a>
